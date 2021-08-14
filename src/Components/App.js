79b122c1f46fe718160e9ea79592a88342641158
ahmedwagd <ahmedwa122@gmail.com>
2021-08-14 06:22:30 +0200

    add google ver

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import youtube from '../Apis/youtube';
 import SearchBar from './SearchBar/SearchBar';
 import VideosList from './VideosList/VideosList';
-
 import './App.css';
 import VideoDetail from './VideoDetail/VideoDetail';
+import GoogleV from './GoogleV';
 
 const Load = () => <div className='text-shadow-pop-tl'>loading ...</div>;
 
@@ -29,22 +30,31 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="ui container">
-        <SearchBar onFormSearch={this.handelSearch} />
-        <div>
-          {this.state.loading ? `i have ${this.state.videos.length} videos.` : <Load />}
-        </div>
-        <div className="ui grid">
-          <div className="ui row">
-            <div className="eleven wide column">
-              <VideoDetail video={this.state.selectedVideo} />
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <div className="ui container">
+              <SearchBar onFormSearch={this.handelSearch} />
+              <div>
+                {this.state.loading ? `i have ${this.state.videos.length} videos.` : <Load />}
+              </div>
+              <div className="ui grid">
+                <div className="ui row">
+                  <div className="eleven wide column">
+                    <VideoDetail video={this.state.selectedVideo} />
+                  </div>
+                  <div className="five wide column">
+                    <VideosList onVideoSelect={this.handelSelectedVideo} videos={this.state.videos} />
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className="five wide column">
-              <VideosList onVideoSelect={this.handelSelectedVideo} videos={this.state.videos} />
-            </div>
-          </div>
-        </div>
-      </div>
+          </Route>
+          <Route path='/google2d9648dfce7a486c.html'>
+            <GoogleV />
+          </Route>
+        </Switch>
+      </Router>
     );
   }
 }
---
+++ src/Components/GoogleV.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const GoogleV = () => {
+  return (
+    <div>
+      google-site-verification: google2d9648dfce7a486c.html
+    </div>
+  )
+}
+
+export default GoogleV;
